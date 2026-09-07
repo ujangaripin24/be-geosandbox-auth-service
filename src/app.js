b2710ae83665dda7510c6ee8b6f7f1e2a25095fe
ujangaripin24 <ujangarifin06@gmail.com>
2026-09-07 13:23:25 +0700

    update config and env

--- src/app.js
+++ src/app.js
@@ -6,49 +6,49 @@ const helmet = require("helmet");
 const createError = require("http-errors");
 const fs = require("fs");
 const path = require("path");
-const database = require('./config/database.config');
-const dataCache = require('./config/data-cache.config');
-const authRouter = require('./routes/auth.route');
-const messageBroker = require('./config/message-broker.config');
+const database = require("./config/database.config");
+const dataCache = require("./config/data-cache.config");
+const authRouter = require("./routes/auth.route");
+const messageBroker = require("./config/message-broker.config");
 
 dotenv.config();
 
 let app = express();
-let accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'))
-let dateNow = new Date().toISOString().replace('T', ' ').substring(0, 19);
+// let accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'))
+let dateNow = new Date().toISOString().replace("T", " ").substring(0, 19);
 
 app.use(helmet());
 app.use(cors());
-app.use(logger('dev'));
-app.use(logger('combined', { stream: accessLogStream }));
+app.use(logger("dev"));
+// app.use(logger('combined', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 200,
-        message: "[SERVICE-AUTH] Server Berhasil Berjalan",
-        date: dateNow
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "[SERVICE-AUTH] Server Berhasil Berjalan",
+    date: dateNow,
+  });
 });
 
-app.use('/api/v1', authRouter);
+app.use("/api/v1", authRouter);
 
 app.use((req, res, next) => {
-    res.status(404).json(createError(404, "Not Found Page you looking for"))
+  res.status(404).json(createError(404, "Not Found Page you looking for"));
 });
 
 app.listen(process.env.APP_PORT, async () => {
-    console.log(`[SERVICE-AUTH] Server berjalan di port ${process.env.APP_PORT}`);
-    try {
-        await database.authenticate();
-        await dataCache.connect();
-        await messageBroker.connectRabbitMQ();
-    } catch (error) {
-        console.error("Unable to start server:");
-        console.error(error.message);
-        process.exit(1);
-    }
+  console.log(`[SERVICE-AUTH] Server berjalan di port ${process.env.APP_PORT}`);
+  try {
+    await database.authenticate();
+    await dataCache.connect();
+    await messageBroker.connectRabbitMQ();
+  } catch (error) {
+    console.error("Unable to start server:");
+    console.error(error.message);
+    process.exit(1);
+  }
 });
 
-module.exports = app
+module.exports = app;
--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || process.env.POSTGRES_USER,
-    password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD,
-    database: process.env.DB_NAME || process.env.POSTGRES_DB,
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || process.env.POSTGRES_PORT || 5432,
-    dialect: 'postgres',
-    logging: false
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
   },
   test: {
-    username: process.env.DB_USER || process.env.POSTGRES_USER,
-    password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD,
-    database: process.env.DB_NAME || process.env.POSTGRES_DB,
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || process.env.POSTGRES_PORT || 5432,
-    dialect: 'postgres',
-    logging: false
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
   },
   production: {
-    username: process.env.DB_USER || process.env.POSTGRES_USER,
-    password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD,
-    database: process.env.DB_NAME || process.env.POSTGRES_DB,
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || process.env.POSTGRES_PORT || 5432,
-    dialect: 'postgres',
-    logging: false
-  }
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  },
 };
